8574f18eb04833943569ecfc364e9369821c3edd
Cesar Valiente <cesar.valiente@gmail.com>
2011-09-22 01:55:46 +0200

    Added license header to all exercises

--- mswl-dt/src/practices/Prac2.py
+++ mswl-dt/src/practices/Prac2.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+''' 
+     Copyright 2011 Cesar Valiente Gordo
+ 
+     This file is part of MSW - Development and Tools exercises.
+
+    This file is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this file.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 '''
 Created on 21/09/2011
 
